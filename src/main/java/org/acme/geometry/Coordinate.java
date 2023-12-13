e72954832a2bf49fd5ca065625118def1483300e
MatRouillard <mathis.rouillard@ensg.eu>
2023-12-13 16:49:50 +0100

    Update Coordinates

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,45 @@
 package org.acme.geometry;
 
 public class Coordinate {
+	
+	private double x;
+	
+	private double y;
+	
+	public Coordinate() {
+		this.x = Double.NaN;
+		this.y = Double.NaN;
+	}
+	
+	public Coordinate(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+	
+	public boolean isEmpty() {
+		return Double.isNaN(x) || Double.isNaN(y);
+	}
+
+	public String toString() {
+		if (Double.isNaN(x) && Double.isNaN(y)) {
+			return "[NaN, NaN]";
+		}
+		else if (Double.isNaN(x)) {
+			return "[NaN,"+y+"]";
+		}
+		else if (Double.isNaN(y)) {
+			return "["+x+",NaN]";
+		}
+		else {
+			return "["+x+","+y+"]";
+		}
+	}
 }
