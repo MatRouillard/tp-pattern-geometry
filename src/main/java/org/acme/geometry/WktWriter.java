ba12caab0ecc443e2f2465b5fd4cfac981db7531
MatRouillard <mathis.rouillard@ensg.eu>
2023-12-15 16:11:08 +0100

    Update WktWriter to be adapted to the correction

--- src/main/java/org/acme/geometry/WktWriter.java
+++ src/main/java/org/acme/geometry/WktWriter.java
@@ -2,34 +2,50 @@ package org.acme.geometry;
 
 public class WktWriter {
 
-	public WktWriter() {
-
-	}
-
 	public String write(Geometry geometry) {
+		StringBuilder buffer = new StringBuilder();
 		if (geometry instanceof Point) {
 			Point point = (Point) geometry;
-			if (point.isEmpty()) {
-				return "POINT EMPTY";
-			} else {
-				return "POINT(" + point.getCoordinate().getX() + " " + point.getCoordinate().getY() + ")";
-			}
+			write(point, buffer);
+			return buffer.toString();
 		} else if (geometry instanceof LineString) {
 			LineString lineString = (LineString) geometry;
-			if (lineString.isEmpty()) {
-				return "LINESTRING EMPTY";
-			} else {
-				String wkt = "LINESTRING(";
-				for (int i = 0; i < lineString.getNumPoints() - 1; i++) {
-					Point p = lineString.getPointN(i);
-					wkt += p.getCoordinate().getX() + " " + p.getCoordinate().getY() + ",";
-				}
-				Point lastPoint = lineString.getPointN(lineString.getNumPoints() - 1);
-				return wkt + lastPoint.getCoordinate().getX() + " " + lastPoint.getCoordinate().getY() + ")";
-			}
+			write(lineString, buffer);
+			return buffer.toString();
 		} else {
 			throw new RuntimeException("geometry type not supported");
 		}
 	}
 
+	private void write(Point point, StringBuilder buffer) {
+		buffer.append("POINT");
+		if (point.isEmpty()) {
+			buffer.append(" EMPTY");
+			return;
+		}
+		buffer.append("(");
+		writeCoordinate(point.getCoordinate(), buffer);
+		buffer.append(")");
+	}
+
+	private void write(LineString lineString, StringBuilder buffer) {
+		buffer.append("LINESTRING");
+		if (lineString.isEmpty()) {
+			buffer.append(" EMPTY");
+			return;
+		}
+		buffer.append("(");
+		for (int i = 0; i < lineString.getNumPoints() - 1; i++) {
+			writeCoordinate(lineString.getPointN(i).getCoordinate(), buffer);
+			buffer.append(",");
+		}
+		writeCoordinate(lineString.getPointN(lineString.getNumPoints() - 1).getCoordinate(), buffer);
+		buffer.append(")");
+	}
+
+	private void writeCoordinate(Coordinate coord, StringBuilder buffer) {
+		double x = coord.getX();
+		double y = coord.getY();
+		buffer.append(x + " " + y);
+	}
 }
